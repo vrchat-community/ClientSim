a509c5566029de7b31f19ae8609dbb99cc018353
cubedparadox <nickbenge615@gmail.com>
2023-06-13 10:03:31 -0700

    fix gitignore, add camera stacking system assets

---
+++ Packages/com.vrchat.ClientSim/Runtime/CameraStacking/ClientSimStackedCamera.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace VRC.SDK3.ClientSim
+{
+    public class ClientSimStackedCamera : ScriptableObject
+    {
+        public string CameraName = "Generic Stacked Camera";
+        public LayerMask RenderLayer;
+        public bool UseOcclusionCulling = true;
+    }
+}
---
+++ Packages/com.vrchat.ClientSim/Runtime/CameraStacking/ClientSimStackedVRCameraSystem.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR;
+using VRC.Core;
+using VRC.SDK3.ClientSim;
+
+namespace VRC.SDK3.ClientSim
+{
+    public class ClientSimStackedVRCameraSystem : MonoBehaviour
+    {
+        public ClientSimStackedCamera[] CameraStack;
+        [HideInInspector] public bool IsCameraStackingEnabled = false;
+        
+        private Camera MainSceneCamera;
+        private bool IsInitialized = false;
+        private bool IsReady = false;
+        private List<Camera> Cameras;
+        
+        private ClientSimMenu clientSimMenu;
+
+        public void Initialize(Camera playerCamera, ClientSimMenu menu)
+        {
+            MainSceneCamera = playerCamera;
+            clientSimMenu = menu;
+        }
+
+        public void Ready()
+        {
+            IsReady = true;
+        }
+        
+        void Update()
+        {
+            if(!IsReady) return;
+            if (!IsInitialized) { InitializeStackedSystem(); }
+            else
+            {
+                if (!Cameras[0].enabled)
+                {
+                    Debug.LogError("Stacked Cameras are not enabled.");
+                }
+            }
+        }
+     
+        /*void OnEnable()
+        {
+            #if !UNITY_ANDROID
+            if (MainSceneCamera != null)
+            {
+                if (!IsCameraStackingEnabled)
+                    CreateCameraStack();
+            }
+            #endif
+        }
+*/
+        void OnDisable()
+        {
+            #if !UNITY_ANDROID
+            if (MainSceneCamera != null)
+            {
+                if (IsCameraStackingEnabled)
+                    DestroyCameraStack();
+            }
+            #endif
+        }
+
+        public void InitializeStackedSystem()
+        {
+            #if !UNITY_ANDROID
+            Cameras = new List<Camera>();
+            if (MainSceneCamera != null)
+            {
+                CreateCameraStack();
+                IsInitialized = true;
+                FormattedLog("Camera Stack has been initialized.");
+            }
+            #else
+            gameObject.SetActive(false);
+            #endif
+        }
+
+        private void CreateCameraStack()
+        {
+            for (int i = 0; i < CameraStack.Length; i++)
+            {
+                AddCamera(i);
+            }
+            IsCameraStackingEnabled = true;
+        }
+
+        private void DestroyCameraStack()
+        {
+            for (int i = 0; i < Cameras.Count; i++)
+            {
+                DestroyCamera(i);
+            }
+            IsCameraStackingEnabled = false;
+        }
+
+        private void AddCamera(int index)
+        {
+            GameObject cameraObj = Instantiate(new GameObject(), MainSceneCamera.transform);
+            Camera cam = cameraObj.AddComponent<Camera>();
+            XRDevice.DisableAutoXRCameraTracking(cam, true);
+
+            cam.CopyFrom(MainSceneCamera); // Start by copying all the settings from the main camera
+            #if VRC_VR_STEAM // We only want this on SteamVR.
+            cameraObj.AddComponent<SteamVRCantedProjectionCullingFix>();
+            #endif
+            Cameras.Add(cam);
+
+            cameraObj.tag = "Untagged";
+            cameraObj.name = $"StackedCamera : {CameraStack[index].CameraName}";
+            cam.clearFlags = CameraClearFlags.Depth;
+            cam.depth = 100 - index;
+            cam.cullingMask = CameraStack[index].RenderLayer;
+            cam.useOcclusionCulling = CameraStack[index].UseOcclusionCulling;
+
+            //Remove this cameras layers from the base camera
+            MainSceneCamera.cullingMask = MainSceneCamera.cullingMask ^ CameraStack[index].RenderLayer;
+            FormattedLog($"Created Stacked Camera : {CameraStack[index].CameraName}");
+            
+            // Set the ClientSim UI canvas to use this camera
+            clientSimMenu.SetCanvasCamera(cam);
+        }
+
+        private void DestroyCamera(int index)
+        {
+            Camera cam = Cameras[index];
+            Cameras.RemoveAt(index);
+
+            //Restore Layers from this camera to the main camera
+            MainSceneCamera.cullingMask = MainSceneCamera.cullingMask | CameraStack[index].RenderLayer;
+            Destroy(cam.gameObject);
+        }
+
+        private void FormattedLog(string s)
+        {
+#if UNITY_EDITOR
+            Debug.Log($"[<color=green>VRC Stacked Camera System</color>] {s}");
+#else
+            Debug.Log($"[VRC Stacked Camera System] {s}");
+#endif
+        }
+    }
+}
