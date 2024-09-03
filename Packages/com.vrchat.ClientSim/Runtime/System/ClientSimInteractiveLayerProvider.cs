91c354bc8ebab8c93357e41de49c9ef88203e740
Yukihiro Tadokoro <nemu90kww@gmail.com>
2024-09-04 08:01:05 +0900

    exclude Passthrough layers from interaction raycasts

--- Packages/com.vrchat.ClientSim/Runtime/System/ClientSimInteractiveLayerProvider.cs
+++ Packages/com.vrchat.ClientSim/Runtime/System/ClientSimInteractiveLayerProvider.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using UnityEngine;
+using VRC.SDKBase;
 
 namespace VRC.SDK3.ClientSim
 {
@@ -16,6 +17,7 @@ namespace VRC.SDK3.ClientSim
         private const int UI_MENU_LAYER = 12;
         private const int INTERNAL_UI_LAYER = 19;
         private const int MIRROR_REFLECTION_LAYER = 18;
+        private const int FIRST_USER_LAYER = 22;
         
         private readonly int _interactiveLayersDefault;
         private readonly int _interactiveLayersUI;
@@ -30,6 +32,8 @@ namespace VRC.SDK3.ClientSim
             _interactiveLayersUI = (1 << UI_LAYER) | (1 << UI_MENU_LAYER) | (1 << INTERNAL_UI_LAYER);
             // When the menu is not open, all layers but UI, UIMenu, and MirrorReflection layers are interactable.
             _interactiveLayersDefault = ~(1 << MIRROR_REFLECTION_LAYER) & ~_interactiveLayersUI;
+            // If Interaction Passthrough is set, these User Layers will also be ignored.
+            _interactiveLayersDefault &= ~(VRC_SceneDescriptor.Instance.interactThruLayers << FIRST_USER_LAYER);
             
             _eventDispatcher = eventDispatcher;
             _eventDispatcher.Subscribe<ClientSimMenuStateChangedEvent>(SetMenuOpen);
